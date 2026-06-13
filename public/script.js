9bbe6a5433ab8d2343c2094d9415cf12505c014b
Prathamesh1878 <prathameshmankar1878@gmail.com>
2026-06-13 12:55:38 +0530

    Added dark mode support

--- public/script.js
+++ public/script.js
@@ -270,3 +270,35 @@ function initMap(lat,lng,city){
           .openPopup();
 
 }
+function toggleTheme(){
+
+    document.body.classList.toggle(
+        "dark-mode"
+    );
+
+    localStorage.setItem(
+        "theme",
+        document.body.classList.contains(
+            "dark-mode"
+        )
+        ? "dark"
+        : "light"
+    );
+
+}
+window.onload = () => {
+
+    const theme =
+    localStorage.getItem(
+        "theme"
+    );
+
+    if(theme === "dark"){
+
+        document.body.classList.add(
+            "dark-mode"
+        );
+
+    }
+
+};
