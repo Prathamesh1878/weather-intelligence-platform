7e40c34843e540fd268e456c7068670a9c0ec81f
Prathamesh1878 <prathameshmankar1878@gmail.com>
2026-06-13 13:40:40 +0530

    Converted application into PWA

--- public/script.js
+++ public/script.js
@@ -439,3 +439,25 @@ function loadHistory(){
     ).innerHTML = html;
 
 }
+if("serviceWorker" in navigator){
+
+    window.addEventListener(
+        "load",
+        () => {
+
+            navigator.serviceWorker
+            .register(
+                "/service-worker.js"
+            )
+            .then(() => {
+
+                console.log(
+                    "Service Worker Registered"
+                );
+
+            });
+
+        }
+    );
+
+}
